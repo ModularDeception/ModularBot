2f97935bb3ac50d3f5ec6b6d45e6e20c88dddfb5
Modular <30032008+ModularDeception@users.noreply.github.com>
2018-03-13 17:14:03 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,14 +4,11 @@ const prefix = "m!";
 
 client.on('ready', () => {
     console.log('I am ready!');
+    var opts = {
+        name: 'with depression',
+        url: 'https://www.twitch.tv/monstercat',
+        type: 1
 });
-
-client.on('ready', () => {
-        var opts = {
-                name: 'with depression',
-                url: 'https://www.twitch.tv/monstercat',
-                type: 1
-        };
 
 client.on('message', message => {
   if (message.content.startsWith(prefix + "ping")) {
