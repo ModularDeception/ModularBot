972e52d49854e6b2a659382775d2896bcb809284
Modular <30032008+ModularDeception@users.noreply.github.com>
2018-03-13 20:55:32 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -46,8 +46,7 @@ client.on('message', message => {
 
 client.on('message', message => {
   if (message.content.startsWith(prefix + "react")) {
-    message.react('🇳') 
-    message.react('🇴')
+    message.react('🦆') 
   }
 });
 
