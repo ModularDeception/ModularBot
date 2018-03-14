0a6b15e3a855835428d0f55fc6c7223dba392d32
Modular <30032008+ModularDeception@users.noreply.github.com>
2018-03-14 15:24:50 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -44,12 +44,4 @@ client.on('message', message => {
   }
 });
 
-client.on('message', message => {
- if (message.content.startsWith(prefix + "say")) {
-    const sayMessage = args.join(" ");
-    message.delete().catch(O_o=>{}); 
-    message.channel.send(sayMessage);
-  }
-});   
-
 client.login(process.env.BOT_TOKEN);
