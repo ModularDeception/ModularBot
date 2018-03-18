48438cc6169ff809582a849ca647e624b80a2ad1
Modular <30032008+ModularDeception@users.noreply.github.com>
2018-03-17 21:33:51 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 const Attachment = require("discord.js").Attachment;
 const client = new Discord.Client();
-const prefix = "m!";
-
+const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+const command = args.shift().toLowerCase();
 
 function isAdmin(member) {
     return member.hasPermission("ADMINISTRATOR");
@@ -62,5 +62,15 @@ client.on('message', message => {
     message.reply('Invite me to your server friend https://rebrand.ly/ModularBot');
   }
 });
+
+ if(command === "say") {
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use.
+    // To get the "message" itself we join the `args` back into a string with spaces:
+    const sayMessage = args.join(" ");
+    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+    message.delete().catch(O_o=>{});
+    // And we get the bot to say the thing:
+    message.channel.send(sayMessage);
+  }
 
 client.login(process.env.BOT_TOKEN);
