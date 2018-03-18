7607335bad4233ca80619aa04e90ce8a06320256
Modular <30032008+ModularDeception@users.noreply.github.com>
2018-03-17 21:36:24 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -62,17 +62,5 @@ client.on('message', message => {
     message.reply('Invite me to your server friend https://rebrand.ly/ModularBot');
   }
 });
-
-client.on("message", async message => {
- if(command === "say") {
-    // makes the bot say something and delete the message. As an example, it's open to anyone to use.
-    // To get the "message" itself we join the `args` back into a string with spaces:
-    const sayMessage = args.join(" ");
-    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-    message.delete().catch(O_o=>{});
-    // And we get the bot to say the thing:
-    message.channel.send(sayMessage);
-  }
-}
-          
+   
 client.login(process.env.BOT_TOKEN);
