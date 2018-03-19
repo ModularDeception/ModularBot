7db10b54d53a2027cbec2b874d5b1c7cae8f6190
Modular <30032008+ModularDeception@users.noreply.github.com>
2018-03-19 19:49:36 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const Attachment = require("discord.js").Attachment;
 const client = new Discord.Client();
-const prefix - "m!";
+const prefix = "m!";
 var ms = require("ms");
 
 function isAdmin(member) {
